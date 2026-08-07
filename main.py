8d2b9d55c5645a41d405ca908ecd054b03fc1e0e
Jack Aram <jack.aram@icloud.com>
2026-08-07 17:13:43 +0100

    added basic code to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+#makes it so the static files load when running the website
+app.mount("/static", StaticFiles(directory="static"), "static")
+
+#displays the website
+@app.get("/")
+async def root():
+    return FileResponse("template/login.html")
